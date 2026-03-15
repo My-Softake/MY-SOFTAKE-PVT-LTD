00ea0ae25a607cc078dc8909e6e62f21e9cc9e0a
mdabutalebdev <mdabutaleb.dev@gmail.com>
2026-03-15 15:49:06 +0600

    language issu fixed

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -3,7 +3,8 @@ import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
   locales: ['en-US', 'en-GB', 'ja', 'zh', 'ko'],
-  defaultLocale: 'en-US'
+  defaultLocale: 'en-US',
+  localeDetection: false, // Domain hit করলে সবসময় en-US (default), ব্রাউজার/cookie দিয়ে en-GB সিলেক্ট হবে না
 });
 
 export const { Link, redirect, usePathname, useRouter } =
